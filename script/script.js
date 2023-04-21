0781eab7bfa3bb6dfd6425617a61895104a71765
JustANipple <prandisamuel99@gmail.com>
2023-04-21 17:36:50 +0200

    modified:   script/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_form.scss

--- script/script.js
+++ script/script.js
@@ -7,14 +7,22 @@ const ageDays = document.querySelector(".age_days span");
 const ageMonths = document.querySelector(".age_months span");
 const ageYears = document.querySelector(".age_years span");
 const errorPars = document.querySelectorAll(".errorPar");
+const form = document.querySelector(".form_birth");
+
+//TO-DO 24-09-1984 = 38 years, 3 months, 26 days
+//TO-DO days must be checked to be right for the month, otherwise show "Must be a valid date"
+//TO-DO days, months and years that overflow the pattern give different errors
+//TO-DO if the field is empty, it shows "this field is required"
 
 submit.addEventListener("click", (event) => {
     event.preventDefault();
-
-    isEmpty();
-
-    if(day.value > daysInMonth(year.value, month.value)) {
-        day.parentElement.setAttribute("class", "alert");
+    
+    const emptyInputs = empty();
+    const overflowInputs = overflow();
+    if(emptyInputs !== 3) {
+        if(overflowInputs !== 3) {
+            setAge();
+        }
     }
 });
 
@@ -32,13 +40,42 @@ function daysInMonth(year, month) {
     return lastDayOfMonth.getDate();
 }
 
-function isEmpty() {
+function empty() {
     const inputs = [day, month, year];
+    let empty = 0;
 
     for(let i = 0; i < inputs.length; i++) {
         if(inputs[i].value === "") {
             inputs[i].parentElement.setAttribute("class", "alert");
             errorPars[i].textContent = "This field is required";
+            empty++;
         }
     }
+
+    return empty;
+}
+
+function overflow() {
+    let overflow = 0;
+
+    if(day.value > 31) {
+        day.parentElement.setAttribute("class", "alert");
+        errorPars[0].textContent = "Must be a valid day";
+        overflow++;
+    } else if (day.value > daysInMonth(year.value, month.value)) {
+        day.parentElement.setAttribute("class", "alert");
+        errorPars[0].textContent = "Must be a valid date";
+        overflow++;
+    }
+    if(month.value > 12) {
+        month.parentElement.setAttribute("class", "alert");
+        errorPars[1].textContent = "Must be a valid month";
+        overflow++;
+    }
+    if(year.value < currentDate.getUTCFullYear) {
+        year.parentElement.setAttribute("class", "alert");
+        errorPars[2].textContent = "Must be in the past";
+        overflow++;
+    }
+    return overflow;
 }
